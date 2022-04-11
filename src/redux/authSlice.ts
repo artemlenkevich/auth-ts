96dcb8fdad5acf47c2ba881909e1468439f85070
Artem Lenkevich <ar.lenkevich@gmail.com>
2022-04-11 16:13:32 +0300

    added state saving in LocalStorage

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { authApi, AuthApi } from "../api"
 
 import { RootState } from "./store"
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./authSlice";
 
+let preloadedState
+const persistedState = localStorage.getItem('state')
+
+if (persistedState) {
+  preloadedState = JSON.parse(persistedState)
+}
+
 export const store = configureStore({
     reducer: {
         auth: authReducer
-    }
+    },
+    preloadedState
+})
+
+window.addEventListener('unload', () => {
+    localStorage.setItem('state', JSON.stringify(store.getState()))
 })
 
 export type RootState = ReturnType<typeof store.getState>
